48fefb6f8e0f95f011f61f9c7810741a91d1c9f0
Rajnish <rajneeshsharma540@gmail.com>
2020-09-22 22:50:16 +0530

    added some changes

--- OneAndTwoPassAssembler.c
+++ OneAndTwoPassAssembler.c
@@ -4,87 +4,89 @@
 #include<ctype.h>
 #include<stdbool.h>
 
+//This is our one pass function
 void onePassAssembler()
 {
-    FILE *f1,*f2;
-	int current_address,starting_address;
-	char opcode[20],label[20],operand[20];
+	FILE *read, *write;
+	int current_address, starting_address;
+	char opcode[20], label[20], operand[20];
 
-	f1=fopen("input.txt","r");
-	f2=fopen("symbol_table.txt","w");
+	read = fopen("InputFile.txt", "r");
+	write = fopen("SymbolTableFile.txt", "w");
 
-	fscanf(f1,"%s %s %s",label,opcode,operand);
-	if(strcmp(opcode,"START")==0)
+	fscanf(read, "%s %s %s", label, opcode, operand);
+	if (strcmp(opcode, "START") == 0)
 	{
-		starting_address=atoi(operand);
+		starting_address = atoi(operand);
 		current_address = starting_address;
 	}
-	while(!feof(f1))
+
+	while (!feof(read))
 	{
-		fscanf(f1,"%s %s %s",label,opcode,&operand);
-		if(strcmp(opcode,"HLT")==0)
+		fscanf(read, "%s %s %s", label, opcode, &operand);
+		if (strcmp(opcode, "HLT") == 0)
+			printf("\n Program length = %d", current_address - starting_address);
+
+		if (strcmp(label, "-") != 0)
 		{
-		    printf("\n Program length = %d",current_address-starting_address);
+			fprintf(write, " %d \t %s \n", current_address, label);
+			printf(" %d \t %s \n", current_address, label);
 		}
-		if(strcmp(label,"-")!=0)
-		{
-			fprintf(f2," %d \t %s \n",current_address,label);
-			printf(" %d \t %s \n",current_address,label);
-		}
-		if(strcmp(opcode,"MOV") == 0 || strcmp(opcode,"SUB") == 0 || strcmp(opcode,"ADD") == 0 || strcmp(opcode,"INR") == 0 || strcmp(opcode,"INX") == 0
-			|| strcmp(opcode,"DCX") == 0 || strcmp(opcode,"CMP") == 0 )
-		{
-			current_address+=1;
-		}
-		else if(strcmp(opcode,"JC") == 0 || strcmp(opcode,"JNZ") == 0 || strcmp(opcode,"JZ") == 0 || strcmp(opcode,"JNC") == 0)
-		{
-			current_address+=3;
-		}
+
+		if (strcmp(opcode, "MOV") == 0 || strcmp(opcode, "SUB") == 0 || strcmp(opcode, "ADD") == 0 || strcmp(opcode, "INR") == 0 || strcmp(opcode, "INX") == 0
+		        || strcmp(opcode, "DCX") == 0 || strcmp(opcode, "CMP") == 0 )
+			current_address += 1;
+
+		else if (strcmp(opcode, "JC") == 0 || strcmp(opcode, "JNZ") == 0 || strcmp(opcode, "JZ") == 0 || strcmp(opcode, "JNC") == 0)
+			current_address += 3;
+
 		else
-		{
-			current_address+=2;
-		}
+			current_address += 2;
 	}
 
-    fclose(f1);
-	fclose(f2);
+	fclose(read);
+	fclose(write);
 }
 
+//This is two pass assembler function
 void twoPassAssembler()
 {
-    FILE *f1,*f2,*f3;
+	FILE *input_read, *output, *symbol_table_read;
 
-    f1=fopen("input.txt","r");
-    int current_address,starting_address;
-	char opcode[20],label[20],operand[20];
+	input_read = fopen("InputFile.txt", "r");
+	int current_address, starting_address;
+	char opcode[20], label[20], operand[20];
 
-	fscanf(f1,"%s %s %s",label,opcode,operand);
+	fscanf(input_read, "%s %s %s", label, opcode, operand);
 
-	if(strcmp(opcode,"START")==0)
+	if (strcmp(opcode, "START") == 0)
 	{
-		starting_address=atoi(operand);
+		starting_address = atoi(operand);
 		current_address = starting_address;
 	}
 
-	while(!feof(f1))
+	while (!feof(input_read))
 	{
-		fscanf(f1,"%s %s %s",label,opcode,operand);
-		if(strcmp(opcode,"HLT")==0)
+		fscanf(input_read, "%s %s %s", label, opcode, operand);
+
+		if (strcmp(opcode, "HLT") == 0)
 		{
-			printf("%d \t 76 \n ",current_address);
-		    printf("\n Program Completed Execution");
-		    break;
+			printf("%d \t 76 \n ", current_address);
+			printf("\n Program Completed Execution");
+			break;
 		}
-		f2=fopen("op_table.txt","r");
-	    f3=fopen("symbol_table.txt","r");
-	    bool flag = false;
-	    char machine_code[20],temp_label[20];
+
+		output = fopen("OutputTableFile.txt", "r");
+		symbol_table_read = fopen("SymbolTableFile.txt", "r");
+
+		bool flag = false;
+		char machine_code[20], temp_label[20];
 		char temp_address[20];
-		while(!feof(f3))
+		while (!feof(symbol_table_read))
 		{
 
-			fscanf(f3,"%s %s",temp_address,temp_label);
-			if(strcmp(operand,temp_label) == 0 )
+			fscanf(symbol_table_read, "%s %s", temp_address, temp_label);
+			if (strcmp(operand, temp_label) == 0 )
 			{
 				flag = true;
 				break;
@@ -92,55 +94,50 @@ void twoPassAssembler()
 		}
 
 		int label_address;
-		if(flag == true)
+		if (flag == true)
 		{
 			label_address = atoi(temp_address);
 //			printf("%d \n",label_address);
 		}
 
-		fclose(f3);
+		fclose(symbol_table_read);
 
-		while(!feof(f2))
+		while (!feof(output))
 		{
-			char temp_opcode[20],temp_machine_code[20],temp_operand[20];
-			fscanf(f2,"%s %s %s",temp_opcode,temp_operand,temp_machine_code);
-			if(strcmp(opcode,temp_opcode) == 0 && (flag == true || strcmp(operand,temp_operand) == 0) )
+			char temp_opcode[20], temp_machine_code[20], temp_operand[20];
+			fscanf(output, "%s %s %s", temp_opcode, temp_operand, temp_machine_code);
+			if (strcmp(opcode, temp_opcode) == 0 && (flag == true || strcmp(operand, temp_operand) == 0) )
 			{
-				strcpy(machine_code,temp_machine_code);
+				strcpy(machine_code, temp_machine_code);
 				break;
 			}
 		}
-		printf("%d \t %s ",current_address,machine_code);
-		if(flag == true)
+		printf("%d \t %s ", current_address, machine_code);
+		if (flag == true)
 		{
-			printf("%d \n",label_address);
-			current_address+=3;
+			printf("%d \n", label_address);
+			current_address += 3;
 		}
 		else
 		{
 			printf("\n");
-			current_address+=1;
+			current_address += 1;
 
 		}
-		fclose(f2);
+		fclose(output);
 	}
 
-
-	fclose(f1);
-
-
-    getch();
-
-
+	fclose(input_read);
+	getch();
 }
+
+//This is our main funciton
 int main()
 {
-     printf("**********One Pass Assembler output****************\n");
-     onePassAssembler();
-     printf("\n\n**********************************************************\n");
-     printf("**********Two Pass Assembler output****************\n");
-     twoPassAssembler();
-
-
-    return 0;
-    }
+	printf("**********One Pass Assembler output****************\n");
+	onePassAssembler();
+	printf("\n\n**********************************************************\n\n");
+	printf("**********Two Pass Assembler output****************\n");
+	twoPassAssembler();
+	return 0;
+}
